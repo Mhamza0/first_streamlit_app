845e4514ec4c649aaa6aff136364313393a70bd5
Muhammad hamza <47133476+Mhamza0@users.noreply.github.com>
2023-11-16 16:06:09 +0500

    Update streamlit_app.py
    Added header 
    Added 3 text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parent New Healthy Diner')
+streamlit.Header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal)
+streamlit.text('Kale , Spinach & Rocket smoothie')
+streamlit.text('Hard Boiled Free-Range Egg)
