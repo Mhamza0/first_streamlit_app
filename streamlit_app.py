43a5252ba301db15db4efc93ac534893fd4901fb
Muhammad hamza <47133476+Mhamza0@users.noreply.github.com>
2023-11-16 16:11:01 +0500

    Update streamlit_app.py
    Emojis added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 streamlit.title('My Parent New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale , Spinach & Rocket smoothie')
-streamlit.text('Hard Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale , Spinach & Rocket smoothie')
+streamlit.text('🐔 Hard Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+   
